7fba0ba8e637d9e5492a942b656e6b2eac92ffca
Mykola Morozov <nickmetal92@gmail.com>
2022-05-15 17:03:59 +0300

    removed actual code

--- account_service/account/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from account.models import Account
-
-
-class AccountAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Account, AccountAdmin)
--- analytic_service/analytics/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# from analytics.models import Analytics
-
-
-class AnalyticsAdmin(admin.ModelAdmin):
-    pass
-
-
-# admin.site.register(Analytics, AnalyticsAdmin)
